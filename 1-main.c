2a2c1687e6c7cb7d726f06f41664d9275eb938b4
gekwealor <gekwealor@gmail.com>
2023-10-03 13:24:16 -0400

    Binary inserts

--- 1-main.c
+++ 1-main.c
@@ -17,7 +17,7 @@ int main(void)
     binary_tree_print(root);
     printf("\n");
     binary_tree_insert_right(root->right, 128);
-    binary_tree_insert_left(root, 54);
+    binary_tree_insert_right(root, 54);
     binary_tree_print(root);
     return (0);
 }
