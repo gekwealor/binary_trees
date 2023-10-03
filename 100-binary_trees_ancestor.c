a17b070c098d691ae114e8e04c6a9e6b185b364c
gekwealor <gekwealor@gmail.com>
2023-10-03 15:23:09 -0400

    Ancestors

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,20 +7,56 @@
   * Return: A pointer of the lowest common ancestor
   * if no ancestor, then NULL.
   */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t
-		*first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+				const binary_tree_t *second)
 {
-	binary_tree_t *mummy, *daddy;
+	size_t base_first, base_second;
 
 	if (!first || !second)
 		return (NULL);
-	if (first == second)
-		return ((binary_tree_t *)first);
 
-	mummy = first->parent, daddy = second->parent;
-	if (first == daddy || !mummy || (!mummy->parent && daddy))
-		return (binary_trees_ancestor(first, daddy));
-	else if (mummy == second || !daddy || (!daddy->parent && mummy))
-		return (binary_trees_ancestor(mummy, second));
-	return (binary_trees_ancestor(mummy, daddy));
+	base_first = binary_tree_base(first);
+	base_second = binary_tree_base(second);
+
+	while (base_first > base_second)
+	{
+		first = first->parent;
+		base_first--;
+	}
+	while (base_second > base_first)
+	{
+		second = second->parent;
+		base_second--;
+	}
+	while (first && second)
+	{
+		if (first == second)
+			return ((binary_tree_t *)first);
+		first = first->parent;
+		second = second->parent;
+	}
+	return ((binary_tree_t *)first);
 }
+
+/**
+  * binary_tree_base - measures the base of a node
+  * @tree: Node to calculate the depth of node
+  * Return: base of the node, 0 if NULL
+  */
+size_t binary_tree_base(const binary_tree_t *tree)
+{
+	size_t base = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		base++;
+		tree = tree->parent;
+	}
+
+	return (base);
+}
+
+
