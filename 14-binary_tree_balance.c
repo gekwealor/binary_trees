0a0815c8b2374364b9c8d2a84e204eaa0f5937a4
gekwealor <gekwealor@gmail.com>
2023-10-04 09:15:39 -0400

    Not really balanced

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,3 +12,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 	return (0);
 }
+
+/**
+  * binary_tree_height - Shows the height of a binary tree.
+  * @tree: A pointer to the root node
+  * Return: NULL, return 0 or else height
+  */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (tree)
+	{
+		size_t l = 0, r = 0;
+
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
+		return ((l > r) ? l : r);
+	}
+	return (0);
+}
