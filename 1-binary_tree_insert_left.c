d793ec3056d6cd1260f92c10b5423c3147479220
gekwealor <gekwealor@gmail.com>
2023-10-04 09:01:39 -0400

    Tree insert

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,20 +11,25 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
+	binary_tree_t *new_node, *old_left_child;
 
 	if (parent == NULL)
 		return (NULL);
 
-	new_node = binary_tree_node(parent, value);
+	new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
-	if (parent->left != NULL)
-	{
-		new_node = parent->left;
-		parent->left->parent = new_node;
-	}
+	old_left_child = parent->left;
+	new_node->n = value;
+	new_node->parent = parent;
+	new_node->left = old_left_child;
+	new_node->right = NULL;
+
+	if (old_left_child != NULL)
+		old_left_child->parent = new_node;
+
 	parent->left = new_node;
 
 	return (new_node);
--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -7,9 +7,9 @@
   */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t power = 0;
-	size_t nodes = 0;
 	size_t height = 0;
+	size_t nodes = 0;
+	size_t power = 0;
 
 	if (!tree)
 		return (0);
@@ -17,29 +17,29 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->right == NULL && tree->left == NULL)
 		return (1);
 
-	power = size_t _power_recursion(2, height + 1);
-	return (power - 1 == nodes);
-
 	height = binary_tree_height(tree);
 	nodes = binary_tree_size(tree);
+
+	power = int is_power_recursion(2, height + 1);
+	return (power - 1 == nodes);
 
 }
 
 /**
-  * _power_recursion - Returns the value of x raised to the power of y
+  * is_power_recursion - Returns the value of x raised to the power of y
   * @x: Exponentiate the value
   * @y: Power to raise x
   * Return: x to the power of y, or -1 if y is negative
   */
 
-int _power_recursion(int x, int y)
+unsigned char is_power_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
 	else
-		return (x * _power_recursion(x, y - 1));
+		return (x * is_power_recursion(x, y - 1));
 }
 
 /**
@@ -68,9 +68,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	height_l = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	height_r = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	height_l = tree->right ? 1 + binary_tree_height(tree->left) : 0;
+	height_r = tree->left ? 1 + binary_tree_height(tree->right) : 0;
 	return (height_l > height_r ? height_l : height_r);
 }
-
-
